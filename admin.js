9b53ba3df2a2e61166c259ac7a4837ce2b313bbb
harissandradesi-glitch <harissandradesi@gmail.com>
2026-01-05 23:23:12 +0700

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,11 +1,30 @@
+const url = "PASTE_URL_SCRIPT_DI_SINI";
+
 function tambahData() {
     let nama = document.getElementById("nama").value;
 
     if (nama === "") {
-        alert("Nama siswa harus diisi!");
-    } else {
-        document.getElementById("hasil").innerHTML =
-            "✅ Data siswa <b>" + nama + "</b> berhasil ditambahkan (contoh).";
-        document.getElementById("nama").value = "";
+        alert("Nama siswa wajib diisi!");
+        return;
     }
+
+    let data = {
+        nama: nama,
+        kelas: "3",
+        keterangan: "Aktif"
+    };
+
+    fetch(url, {
+        method: "POST",
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(result => {
+        alert("✅ Data berhasil disimpan ke Google Sheets");
+        document.getElementById("nama").value = "";
+    })
+    .catch(error => {
+        alert("❌ Gagal menyimpan data");
+        console.error(error);
+    });
 }
